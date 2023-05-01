5c03bf9777835533fd496c8f7255baffc3600ce6
adva-mo <advamozes@gmail.com>
2023-05-01 18:06:37 +0300

    added api route for users

---
+++ app/api/users/index.js
@@ -0,0 +1,27 @@
+import { createUser, getUsers } from "@/lib/prisma/users";
+
+const handler = async (req, res) => {
+  if (req.method === "GET") {
+    try {
+      const { users, error } = await getUsers();
+      if (error) throw new Error(error);
+      return res.status(200).json({ users });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  } else if (req.method === "POST") {
+    try {
+      const data = req.body;
+      // verify data
+      const { user, error } = await createUser(data);
+      if (error) throw new Error(error);
+      return res.status(200).json({ user });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
+  res.setHeader("Allow", ["GET", "POST"]);
+  res.status(425).end(`Method ${req.method} is not allowed`);
+};
+
+export default handler;
