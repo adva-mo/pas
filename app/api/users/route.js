c14bf0cc004c0aa3ef7cbc6d1b2b5f852d03d4ac
adva-mo <advamozes@gmail.com>
2023-05-02 14:05:01 +0300

    Refactor for user route functions

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -7,19 +7,20 @@ export async function GET(req) {
     if (error) throw new Error(error);
     return NextResponse.json({ users });
   } catch (error) {
-    console.error(error);
-    // return res.status(500).json({ error: error.message });
+    console.error(error.message);
+    return NextResponse.json({ error: error.message });
   }
 }
 
 export async function POST(req) {
   try {
     const data = await req.json();
-    //todo verify data isnt null
+    //todo: send feedback of no body in req
     const { user, error } = await createUser(data);
     if (error) throw new Error(error);
     return NextResponse.json({ user });
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    console.error(error.message);
+    return NextResponse.json({ error: error.message });
   }
 }
