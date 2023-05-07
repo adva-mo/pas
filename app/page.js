84a6947b97a90927fb41b1e32ff61dcd29d70fe1
adva-mo <advamozes@gmail.com>
2023-05-07 15:22:12 +0300

    home page design

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
+import logo from "../public/pas-logo.png";
+
+const username = "יותם";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      PAS
+    <main className="flex min-h-screen flex-col items-center py-6">
+      <Image src={logo} height={50} alt="Picture of the author" />
+      <h2 className="mt-6"> שלום, {username}</h2>
+      <div>
+        <Link href={"/about"}>הוסף נוכחות</Link>
+      </div>
     </main>
   );
 }
