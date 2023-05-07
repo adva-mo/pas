d2d090fcbf0075d5e35fde14268f78f7ddf9f2ad
adva-mo <advamozes@gmail.com>
2023-05-07 14:19:38 +0300

    Added route to fetch attendances

---
+++ app/api/attendance/[id]/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import { getAttendanceById } from "@/lib/prisma/attendance";
+
+//TODO convert this route to /search?uid=?pid
+export async function GET(request, { params }) {
+  try {
+    const { uid } = params;
+    const { attendances, error } = await getAttendanceById(uid);
+    if (error) throw new Error(error);
+    return NextResponse.json({ attendances });
+  } catch (error) {
+    console.error(error.message);
+    return NextResponse.json({ error: error.message });
+  }
+}
--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -13,7 +13,6 @@ export async function GET(req) {
 }
 
 export async function POST(req) {
-  console.log("posting projcet");
   try {
     const data = await req.json();
     //todo: send feedback of no body in req
