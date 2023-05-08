43d7a83a8f7c29dd85e3a7298e248e8e41f20ecd
adva-mo <advamozes@gmail.com>
2023-05-08 19:53:49 +0300

    Added form to get attendance

--- app/newattendance/page.js
+++ app/newattendance/page.js
@@ -1,5 +1,35 @@
 function newAttendance() {
-  return <form>new attendance</form>;
+  return (
+    <form>
+      <div>
+        <label>עובד</label>
+        <select>בחר עובד</select>
+      </div>
+      <div>
+        <label>פרוייקט</label>
+        <select>בחר פרוייקט</select>
+      </div>
+      <div>
+        <label>תאריך</label>
+        <input />
+      </div>
+      <div>
+        <label>הספק</label>
+        <input />
+      </div>
+      <div>
+        <label>תשלום יומי</label>
+        <input typeof="number" />
+      </div>
+      <div>
+        <label>הערות</label>
+        <input />
+      </div>
+      <button>הוספה</button>
+    </form>
+  );
 }
 
 export default newAttendance;
+
+//fetch open projects and users
