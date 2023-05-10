363650c7b1c7fe2056684c59e9aef902b7902ae5
adva-mo <advamozes@gmail.com>
2023-05-10 17:07:25 +0300

    Creating new users from the client components

--- app/newuser/page.js
+++ app/newuser/page.js
@@ -1,14 +1,35 @@
-function newUser(params) {
+"use client";
+import axios from "axios";
+import { useRef } from "react";
+
+export default function newUser() {
+  const oidRef = useRef();
+  const nameRef = useRef();
+
+  const saveUser = async () => {
+    const newUser = {
+      oid: Number(oidRef.current.value),
+      name: nameRef.current.value,
+      password: oidRef.current.value,
+    };
+    const { data } = await axios.post(
+      "http://localhost:3000/api/users",
+      newUser
+    );
+    console.log(data);
+  };
+
   return (
-    <form>
+    <form className="text-right" onSubmit={(e) => e.preventDefault()}>
       <div>
-        <input></input>
-        <label>ת.ז.</label>
-      </div>
-      <div>
-        <input></input>
+        <input ref={nameRef} />
         <label>שם</label>
       </div>
+      <div>
+        <input ref={oidRef} />
+        <label>ת.ז.</label>
+      </div>
+      <button onClick={() => saveUser()}>שמירה</button>
     </form>
   );
 }
