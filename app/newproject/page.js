a7201613b1f8c42c450e6389c521d452c17cf495
adva-mo <advamozes@gmail.com>
2023-05-10 15:40:07 +0300

    Fetching clients from new project page

--- app/newproject/page.js
+++ app/newproject/page.js
@@ -1,26 +1,34 @@
-function newProject() {
+import { getClients } from "@/lib/prisma/clients";
+
+async function newProject() {
+  const clients = await getClients();
+
   return (
     <form>
       <div>
+        {clients && console.log(clients)}
         <label>name</label>
-        <input></input>
+        <input />
       </div>
       <div>
         <label>adress</label>
-        <input></input>
+        <input />
       </div>
       <div>
-        <label>clientId</label>
-        <select>לקוח</select>
+        <label>לקוח</label>
+        <select>
+          <option value={"בחר לקוח"}>בחר לקוח</option>
+          {clients.map((c) => (
+            <option value={c.name}>{c.name}</option>
+          ))}
+        </select>
       </div>
       <div>
-        <label>startedAt</label>
-        <select>נפתח בתאריך</select>
+        <label>נפתח בתאריך</label>
+        <input />
       </div>
     </form>
   );
 }
 
 export default newProject;
-
-// fetch clients id's
