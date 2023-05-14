27be4faaadf87a475317bdef5bb49cdbf3ea9431
adva-mo <advamozes@gmail.com>
2023-05-14 19:40:17 +0300

    Added functions to create new client in DB

--- app/newclient/page.js
+++ app/newclient/page.js
@@ -1,14 +1,35 @@
+"use client";
+import axios from "axios";
+import { useRef } from "react";
+
 function newClient() {
+  const formRef = useRef();
+
+  const saveClient = async () => {
+    try {
+      const newClient = Object.fromEntries(new FormData(formRef.current));
+      formRef.current.reset();
+      const { data } = await axios.post("http://localhost:3000/api/clients", {
+        ...newClient,
+        oid: Number(newClient.oid),
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form>
+    <form ref={formRef} onSubmit={(e) => e.preventDefault()}>
       <div>
-        <input></input>
-        <label>ח.פ.</label>
+        <input name="oid"></input>
+        <label htmlFor="oid">ח.פ.</label>
       </div>
       <div>
-        <input></input>
-        <label>שם</label>
+        <input name="name"></input>
+        <label htmlFor="name">שם</label>
       </div>
+      <button onClick={() => saveClient()}>שמירה</button>
     </form>
   );
 }
