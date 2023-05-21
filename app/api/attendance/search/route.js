358cf848f3aac558211d3f64096d48736c1052e6
adva-mo <advamozes@gmail.com>
2023-05-21 11:25:50 +0300

    changed the serach route to include search params

--- app/api/attendance/search/route.js
+++ app/api/attendance/search/route.js
@@ -2,19 +2,19 @@ import { NextResponse } from "next/server";
 import { getAttendanceById } from "@/lib/prisma/attendance";
 
 //TODO convert this route to /search?uid=?pid
-export async function GET(request, { params }) {
+export async function GET(request) {
   try {
-    const { uid } = params;
-    const { attendances, error } = await getAttendanceById(uid);
-    if (error) throw new Error(error);
-    return NextResponse.json({ attendances });
+    const { searchParams } = new URL(request.url);
+    const uid = searchParams.get("uid");
+    const pid = searchParams.get("pid");
+    const from = searchParams.get("from");
+    const to = searchParams.get("to");
+
+    const filtered = await getAttendanceById(uid, pid, from, to);
+
+    return NextResponse.json({ attendances: filtered });
   } catch (error) {
     console.error(error.message);
     return NextResponse.json({ error: error.message });
   }
 }
-
-//uid
-//pid
-//from
-//to
