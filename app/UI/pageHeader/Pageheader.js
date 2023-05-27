c58fc7e2a47aa20aae42358a305d98f3fc9f18e4
adva-mo <advamozes@gmail.com>
2023-05-27 09:42:53 +0300

    Added link to homepage on header

--- app/UI/pageHeader/Pageheader.js
+++ app/UI/pageHeader/Pageheader.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 function PageHeader({ title }) {
   return (
     <div className="bg-sky-400 w-full h-20 flex justify-end items-center gap-4">
       <h4>{title}</h4>
-      <button>חזור</button>
+      <Link href="/">חזור</Link>
     </div>
   );
 }
