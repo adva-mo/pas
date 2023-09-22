8528fb1d1de5ccc9e5ecd2813c73f9ba4acdef5c
adva-mo <advamozes@gmail.com>
2023-09-22 13:52:49 +0300

    cors config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: `${process.env.ALLOWED_ORIGIN}`,
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
